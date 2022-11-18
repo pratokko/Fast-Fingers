b8a370d9638f5592a8365c375857e39a414e65bf
pratokko <pratokko3@gmail.com>
2022-11-18 19:52:00 +0300

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>Reaction time: {{ score }} ms</div>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = " Superfast reflexes";
+    } else if (this.score < 400) {
+      this.rank = "Fast Fingers";
+    } else {
+      this.rank = "Snail pace...";
+    }
+  },
+};
+</script>
+
+<style>
+
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
